d2da1f446086577ff11e43b01ada84629082953b
Jakub Zając <jakubtomaszkrzysztof@gmail.com>
2025-03-13 13:18:10 +0100

    Lab2 done

---
+++ lab2/collatz/collatz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int collatz_conjecture(int input) {
+    if (input % 2 == 0) {
+        return input / 2;
+    } else {
+        return 3 * input + 1;
+    }
+}
+
+int test_collatz_convergence(int input, int max_iter, int *steps) {
+    int count = 0;
+    while (input != 1 && count < max_iter) {
+        steps[count++] = input;
+        input = collatz_conjecture(input);
+    }
+    if (input == 1) {
+        steps[count++] = 1;
+        return count;
+    }
+    return 0;
+}
